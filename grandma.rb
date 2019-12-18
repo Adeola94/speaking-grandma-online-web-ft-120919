4a6ca5e778a3c9d94e23f5b88e6c11cb73c01dd6
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-12-18 05:05:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,18 +2,15 @@ def speak_to_grandma
 
 if speak_to_grandma = "I LOVE YOU GRANDMA"
 puts  "I LOVE YOU TOO PUMPKIN!"
-end 
-if speak_to_grandma = 'Hi Nana, how are you?'
+elsif speak_to_grandma = 'Hi Nana, how are you?'
   puts "HUH?! SPEAK UP, SONNY!"
-end 
-if speak_to_grandma = "Hi!"
+elsif speak_to_grandma = "Hi!"
    puts "HUH?! SPEAK UP, SONNY!"
- end
- if speak_to_grandma = 'WHAT DID YOU EAT TODAY?'
+ elsif speak_to_grandma = 'WHAT DID YOU EAT TODAY?'
+   puts "NO, NOT SINCE 1938!"
+ elsif speak_to_grandma = 'WHAT?'
    puts "NO, NOT SINCE 1938!"
  end 
- if speak_to_grandma = 'WHAT?'
-   puts "NO, NOT SINCE 1938!"
  end 
  
  
